2d00872dc743100151a72b622563145bbda0289e
Amiel Iliesi <Amiel Iliesi>
2017-04-27 12:08:18 -0700

    added database handling into the setup; correctly imports dbm for the exe now

--- senior project/code and resources/setup.py
+++ senior project/code and resources/setup.py
@@ -2,9 +2,11 @@ import sys
 from cx_Freeze import setup, Executable
 
 # Dependencies are automatically detected, but it might need fine tuning.
-build_exe_options = {"optimize": 2, }
+build_exe_options = {"optimize": 2,
+                     "packages": ["dbm"]}
 
 setup(  name = "Tetris Code",
         version = "3.6",
+        options = {"build_exe": build_exe_options},
         description = "My take on Tetris.",
         executables = [Executable("tetris_code.py", base="Win32GUI")])
